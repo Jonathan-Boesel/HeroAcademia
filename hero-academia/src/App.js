da4c3dffdaadddb0f54c3841cc389ad0d743bdef
Jonathan-Boesel <jboesel@vt.edu>
2019-08-18 16:56:17 -0400

    Initial Setup

---
+++ hero-academia/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+
+      }
+    }
+
+  //Functions 
+
+  //Render - Contains elements and lifted state
+  render() {
+    return(
+    
+      <div>
+        <span>"Test"</span>
+      </div>
+    )
+  }
+}
+export default App;
